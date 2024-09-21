bb1034e1711c5e43029a2ae84b054e096d0aeb21
Ahmed Samir <a.samir9710@gmail.com>
2024-09-21 21:13:28 +0300

    enhance blog project structure and add routes struct

--- services/blogs/internal/repository/sql.go
+++ services/blogs/internal/repository/sql.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"blogs/config"
+	"blogs/internal/domain"
 	"database/sql"
 	"log"
 )
@@ -22,3 +23,24 @@ func NewSqlRepository() *SqlRepository {
 	return &SqlRepository{Db: db}
 }
 
+func (bs *SqlRepository) Create(user domain.Blog) (domain.Blog, error) {
+
+	return user, nil
+}
+
+func (bs *SqlRepository) Update(updatedUser domain.Blog, id string) (domain.Blog, error) {
+
+	return updatedUser, nil
+}
+
+func (bs *SqlRepository) Delete(id string) error {
+	return nil
+}
+
+func (bs *SqlRepository) GetByID(id string) *domain.Blog {
+	return nil
+}
+
+func (bs *SqlRepository) List() []domain.Blog {
+	return nil
+}
