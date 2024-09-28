0c388f5c875d742efecffafe4a3708d9371546d3
Ahmed Samir <a.samir9710@gmail.com>
2024-09-28 08:08:42 +0300

    core(analytics): add kafka consumer in analytics service to consumer blog viewed event from blogs service

--- analytics-service/app/main.py
+++ analytics-service/app/main.py
@@ -1,9 +1,16 @@
+import asyncio
+import logging
 from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from messaging.consumer import start_consumer
+
 app = FastAPI()
+
+loop = asyncio.get_event_loop()
+logging.basicConfig(level=logging.INFO)
 
 
 class Item(BaseModel):
@@ -25,3 +32,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}/")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+@app.on_event("startup")
+async def startup_event():
+    loop.create_task(start_consumer(loop))
---
+++ analytics-service/app/repository/sqlalchemy.py
@@ -0,0 +1,18 @@
+from .db import AbstractRepository
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def create(self, entity):
+        return super().create(entity)
+
+    def list(self, filters=None):
+        return super().list(filters)
+
+    def get(self, id):
+        return super().get(id)
+
+    def update(self, entity):
+        return super().update(entity)
+
+    def delete(self, id):
+        return super().delete(id)
